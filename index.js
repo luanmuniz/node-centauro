927fa8fa43aed14ad00718ceb6a919c7824ab61f
Luan Muniz <luan@luanmuniz.com.br>
2015-12-03 11:12:56 -0200

    Expose functions && configs

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 
 var catalogAPI = require('./lib/catalog'),
 	coreAPI = require('./lib/api/core'),
-	skuAPI = require('./lib/api/sku');
+	skuAPI = require('./lib/api/sku'),
+	trackingAPI = require('./lib/api/tracking'),
+	orderAPI = require('./lib/api/order');
 
 module.exports = function(keys, env) {
 	var centauroCore = coreAPI.init(keys, env);
 
 	this.sku = skuAPI.init(centauroCore);
+	this.tracking = trackingAPI.init(centauroCore);
+	this.order = orderAPI.init(centauroCore);
+
 	if(keys.partnerName) {
 		this.catalog = catalogAPI.init(keys.partnerName, env);
 	}
